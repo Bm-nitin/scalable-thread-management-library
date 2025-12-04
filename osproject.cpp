bec42e225ffdc9058faf55b59866e72a802c72b2
Nitin Kumar <nitinkumar.2024@lpu.in>
2025-12-04 13:21:44 +0530

    feat: added basic ThreadPool with worker threads and task queue

--- osproject.cpp
+++ osproject.cpp
@@ -1,24 +1,77 @@
 #include <iostream>
 #include <thread>
+#include <vector>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <functional>
 using namespace std;
 
-// Simple function for threads
-void printTask(int id) {
-    cout << "Task " << id 
-         << " running in thread " 
-         << this_thread::get_id() << endl;
-}
+class ThreadPool {
+private:
+    vector<thread> workers;
+    queue<function<void()>> tasks;
+    mutex mtx;
+    condition_variable cv;
+    bool stop = false;
+
+public:
+    ThreadPool(int count) {
+        for (int i = 0; i < count; i++) {
+            workers.emplace_back([this]() {
+                while (true) {
+                    function<void()> task;
+
+                    {
+                        unique_lock<mutex> lock(mtx);
+                        cv.wait(lock, [this]() {
+                            return stop || !tasks.empty();
+                        });
+
+                        if (stop && tasks.empty())
+                            return;
+
+                        task = tasks.front();
+                        tasks.pop();
+                    }
+
+                    task();
+                }
+            });
+        }
+    }
+
+    void addTask(function<void()> newTask) {
+        {
+            lock_guard<mutex> lock(mtx);
+            tasks.push(newTask);
+        }
+        cv.notify_one();
+    }
+
+    void shutdown() {
+        {
+            lock_guard<mutex> lock(mtx);
+            stop = true;
+        }
+        cv.notify_all();
+
+        for (auto &t : workers)
+            t.join();
+    }
+};
 
 int main() {
-    // Create two simple threads
-    thread t1(printTask, 1);
-    thread t2(printTask, 2);
+    ThreadPool pool(3);
 
-    // Wait for threads to finish
-    t1.join();
-    t2.join();
+    for (int i = 1; i <= 5; i++) {
+        pool.addTask([i]() {
+            cout << "Task " << i
+                 << " running in thread "
+                 << this_thread::get_id() << endl;
+        });
+    }
 
-    cout << "Initial thread test completed." << endl;
+    pool.shutdown();
     return 0;
 }
-
